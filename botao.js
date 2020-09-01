ec9a0351b45858a96fd2effe6c2b958a287d9b1d
MylenaAmorim <mylenaamorim13@gmail.com>
2020-09-01 15:47:48 -0300

    desenvolvimento

---
+++ botao.js
@@ -0,0 +1,21 @@
+function Nova()
+{
+location.href="index.html"
+}
+
+function Js()
+{
+location.href="scr/js.html"
+}
+function Hc()
+{
+location.href="scr/hc.html"
+}
+function Py()
+{
+location.href="scr/python.html"
+}
+function Jv()
+{
+location.href="scr/java.html"
+}
